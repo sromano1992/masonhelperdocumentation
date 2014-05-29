a05ecbec57abd7d833112cecf502da174a910e5b
simrom <s.romano1992@gmail.com>
2014-05-29 15:54:45 +0200

    Externalized string

---
+++ src/it/isislab/masonhelperdocumentation/mason/wizards/Messages.java
@@ -0,0 +1,26 @@
+package it.isislab.masonhelperdocumentation.mason.wizards;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "it.isislab.masonhelperdocumentation.mason.wizards.messages"; //$NON-NLS-1$
+	public static String AgentPositionPhrase_1;
+	public static String AgentPositionPhrase_2;
+	public static String AgentPositionPhrase_3;
+	public static String GridsCell_heigthIs;
+	public static String GridsCell_HeigthValue;
+	public static String GridsCell_ValueOf;
+	public static String GridsCell_widthIs;
+	public static String GridsCell_WidthValue;
+	public static String Initializer_2;
+	public static String InitialValuePhrase_1;
+	public static String OtherVar_1;
+	public static String OtherVar_2;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
---
+++ src/it/isislab/masonhelperdocumentation/visitor/Messages.java
@@ -0,0 +1,56 @@
+package it.isislab.masonhelperdocumentation.visitor;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "it.isislab.masonhelperdocumentation.visitor.messages"; //$NON-NLS-1$
+	public static String StartMethodVisitor_AddEdgeInfo1;
+	public static String StartMethodVisitor_AddEdgeInfo2;
+	public static String StartMethodVisitor_AddEdgeInfo3;
+	public static String StartMethodVisitor_AddEdgeInfo4;
+	public static String StartMethodVisitor_AddEdgeInfo5;
+	public static String StartMethodVisitor_AddEdgeInformation;
+	public static String StartMethodVisitor_AddNode1;
+	public static String StartMethodVisitor_AllObj1;
+	public static String StartMethodVisitor_Assignemnt1;
+	public static String StartMethodVisitor_Assignment;
+	public static String StartMethodVisitor_Do1;
+	public static String StartMethodVisitor_DoWhileEnd;
+	public static String StartMethodVisitor_EndAssignent;
+	public static String StartMethodVisitor_EndFor;
+	public static String StartMethodVisitor_EndIf;
+	public static String StartMethodVisitor_EndInvocation;
+	public static String StartMethodVisitor_EndInvocation1;
+	public static String StartMethodVisitor_EndMethod;
+	public static String StartMethodVisitor_EndSwitch;
+	public static String StartMethodVisitor_EndTypeDeclaration;
+	public static String StartMethodVisitor_GetObj1;
+	public static String StartMethodVisitor_GetObj2;
+	public static String StartMethodVisitor_If1;
+	public static String StartMethodVisitor_IntRandom;
+	public static String StartMethodVisitor_InvokeMethod;
+	public static String StartMethodVisitor_MutliplesInfo2;
+	public static String StartMethodVisitor_MutlipliesInfo1;
+	public static String StartMethodVisitor_RadomDouble1;
+	public static String StartMethodVisitor_Schedule1;
+	public static String StartMethodVisitor_Schedule2;
+	public static String StartMethodVisitor_Schedule3;
+	public static String StartMethodVisitor_Schedule4;
+	public static String StartMethodVisitor_Schedule5;
+	public static String StartMethodVisitor_SeeSubmodel;
+	public static String StartMethodVisitor_SetObjLoc1;
+	public static String StartMethodVisitor_SetObjLoc2;
+	public static String StartMethodVisitor_SetObjLoc3;
+	public static String StartMethodVisitor_SetObjLoc4;
+	public static String StartMethodVisitor_SubmodelFound;
+	public static String StartMethodVisitor_SwitchInfo;
+	public static String StartMethodVisitor_TypeDeclaration;
+	public static String StartMethodVisitor_While1;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
